2f2c17db2e27c7035b12f9cd754bd7a6c84fe77a
Carmani H. Jackson <charrisjackson422@gmail.com>
2025-02-11 13:50:06 -0500

    pushing so everyone can get update json and node modules

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,21 @@
 import '../App.css'
+import {Flame, Menu, UserRound} from 'lucide-react';
 
 const Account = () => {
     return (
         <>
             <h1>Account Page</h1>
+            <Flame  />
+            <h1>Streak</h1>
+            <p>You have a streak of 5 days!</p>
+            <Menu />
+            <h1>platinum quizess</h1>
+            <p>golan - intermediate</p>
+            <p>Javascript - beginner</p>
+            <p>AWS - beginner</p>
+            <UserRound />
+            <h1>lrnr Level:2</h1>
+            <p>150/200 xp</p>
         </>
     )
 }
