fc670e3d9757341adee164dd936629cfce251d1f
Ismael <imartinez@r2happrentice.org>
2025-02-11 23:50:03 -0500

    quiz static page vol_1

---
+++ src/components/EvalCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Eval() {
+  return (
+    <div className="evalGen text-teal-500 p-4 m-4 flex ">
+      <div>
+        <h2 className="text-2xl mb-4 mr-40">Verners Evaluation</h2>
+        <h2 className="text-2xs mb-4 text-black ">Correct Answer</h2>
+      </div>
+      <p className="text-black w-1/2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nobis
+        rerum cupiditate quaerat autem id delectus pariatur qui modi sed nihil
+        numquam aspernatur vel animi esse obcaecati, amet asperiores eius? Nulla
+        praesentium temporibus voluptas, consequuntur assumenda libero
+        distinctio unde, laboriosam quisquam quas ut rem dolores eligendi
+        ratione voluptatibus quos ipsa!
+      </p>
+    </div>
+  );
+}
