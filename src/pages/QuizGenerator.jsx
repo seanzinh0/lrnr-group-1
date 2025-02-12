eec003a5960031a84d6f5d75d42844ff99dbf3fd
‘SkyPB’ <skyshanice01@gmail.com>
2025-02-12 16:16:44 -0500

    merge branches

--- src/pages/QuizGenerator.jsx
+++ src/pages/QuizGenerator.jsx
@@ -1,64 +1,43 @@
 import "../App.css";
-import '../App.css'
-import QuizDropdown from '../components/QuizDropdown'
+import "../App.css";
+import QuizDropdown from "../components/QuizDropdown";
 
 const QuizGenerator = () => {
+  const topics = ["Goland", "AWS", "Javascript", "CI/CD"];
+
+  const difficulty = ["Novice", "Intermediate", "Expert"];
+
+  const questionAmount = [5, 10, 15];
+
+  const style = [
+    "Master Oogway",
+    "1940s Gangster",
+    "Like I am 8 years old",
+    "Jedi",
+    "Goku",
+  ];
+
   return (
     <>
-      <h1>Quiz Generator Page</h1>
+      <div class="flex flex-col p-10">
+        <h1 class="text-4xl">Quiz Generation Options</h1>
+        <p class="pt-10">
+          Please choose your preferences below to generate your personalized
+          quiz.
+        </p>
+        <div class="space-y-5">
+          <h1 class="mt-10 text-gray-500">Topic</h1>
+          <QuizDropdown data={topics} />
+          <h1 class="text-gray-500">Expertise</h1>
+          <QuizDropdown data={difficulty} />
+          <h1 class="text-gray-500">Number of Questions</h1>
+          <QuizDropdown data={questionAmount} />
+          <h1 class="text-gray-500">Style of questions</h1>
+          <QuizDropdown data={style} />
+        </div>
+      </div>
     </>
   );
 };
-
-    const topics = [
-        'Goland',
-        'AWS',
-        'Javascript',
-        'CI/CD'
-    ]
-
-    const difficulty = [
-        'Novice',
-        'Intermediate',
-        'Expert'
-    ]
-
-    const questionAmount = [
-        5,
-        10,
-        15
-    ]
-
-    const style = [
-        'Master Oogway',
-        '1940s Gangster',
-        'Like I am 8 years old',
-        'Jedi', 
-        'Goku',
-    ]
-
-
-    return (
-        <>
-
-            <div class='flex flex-col p-10'>
-
-                <h1 class='text-4xl'>Quiz Generation Options</h1>
-                <p class='pt-10'>Please choose your preferences below to generate your personalized quiz.</p>
-                <div class='space-y-5'>
-                    <h1 class='mt-10 text-gray-500'>Topic</h1>
-                    <QuizDropdown data={topics}/>
-                    <h1 class='text-gray-500'>Expertise</h1>
-                    <QuizDropdown data={difficulty}/>
-                    <h1 class='text-gray-500'>Number of Questions</h1>
-                    <QuizDropdown data={questionAmount} />
-                    <h1 class='text-gray-500'>Style of questions</h1>
-                    <QuizDropdown data={style} />
-                </div>
-            </div>
-
-        </>
-    )
-}
 
 export default QuizGenerator;
