467cf933d3e07a80b7372cebe533ffc2a710060b
Ismael <imartinez@r2happrentice.org>
2025-02-12 13:36:39 -0500

    quiz page vol_2

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 
 export default function Questions({ question }) {
+  const [isFocused, setIsFocused] = React.useState(false);
+
   return (
     <>
-      <div className="questionGen text-teal-500 p-4 m-4">
-        <h1 className="text-2xl">Question</h1>
-        <p className="text-black m-4 ml-0">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, eius.
-        </p>
+      <div className="questionGen text-teal-500 p-4 w-11/12 flex flex-col">
+        <div>
+          <h1 className="text-4xl">Question</h1>
+          <p className="text-black m-4 ml-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+            eius.
+          </p>
 
-        <h2>Your Answer</h2>
-        <input type="text" placeholder="answer" />
+          <h2 className="text-4xl">Your Answer</h2>
+          <div className="flex flex-col ml-2 mt-10">
+            <label
+              className={`block mb-1 ${
+                isFocused ? "text-teal-500" : "text-gray-500"
+              }`}
+            >
+              Answer
+            </label>
+            <input
+              type="text"
+              className="border-b-2 border-black w-11/12 outline-0 focus:border-teal-500"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+          </div>
+        </div>
+        <button className="bg-teal-500 rounded text-white text-xs border-solid mt-10 p-3 rounded-1 w-1/12">
+          SUBMIT ANSWER
+        </button>
       </div>
     </>
   );
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -7,14 +7,13 @@ const Quiz = () => {
   return (
     <>
       <h1 className="text-teal-500 text-4xl text-center mt-5">1 of 5</h1>
-      <Questions />
-      <button className="bg-teal-500 text-white text-xs border-solid  ml-8 p-3 rounded-1">
-        SUBMIT ANSWER
-      </button>
-      <Eval />
-      <button className="bg-teal-500 text-white text-xs border-solid mb-8 ml-8 p-3 rounded-1">
-        NEXT
-      </button>
+      <div className="mb-44 flex justify-center">
+        <Questions />
+      </div>
+
+      <div className="mb-72 flex justify-center">
+        <Eval />
+      </div>
     </>
   );
 };
