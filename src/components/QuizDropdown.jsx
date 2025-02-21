4a4d82161f8aef0ede7eba6ba3c59ce9c0f51d1c
‘SkyPB’ <skyshanice01@gmail.com>
2025-02-21 15:23:18 -0500

    test quiz answers

--- src/components/QuizDropdown.jsx
+++ src/components/QuizDropdown.jsx
@@ -1,25 +1,27 @@
-
-
 //creates props that can be passed through to create dropdown options
-const QuizDropdown = (props) => {
+const QuizDropdown = ({ data, onSelect }) => {
+  //maps each option
+  const selectOptions = data.map((option) => (
+    <option className="text-gray-900 p-3" key={option} value={option}>
+      {option}
+    </option>
+  ));
 
-    //maps each option
-    const selectOptions = props.data.map(option => (
-        <option className="text-gray-900 p-3" key={option} value={option}>
+  const handleChange = (e) => {
+    onSelect(e.target.value);
+  };
 
-            {option}
-
-        </option>
-    ))
-
-    return (
-        <>
-            <select className="w-11/12 p-3 border-b-2 border-gray-300 focus:outline-0 focus:border-blue-500 bg-transparent">
-                <option></option>
-                {selectOptions}
-            </select>
-        </>
-    ) 
-}
+  return (
+    <>
+      <select
+        className="w-11/12 p-3 border-b-2 border-gray-300 focus:outline-0 focus:border-blue-500 bg-transparent"
+        onChange={handleChange}
+      >
+        <option value="">Select an option</option>
+        {selectOptions}
+      </select>
+    </>
+  );
+};
 
 export default QuizDropdown;
