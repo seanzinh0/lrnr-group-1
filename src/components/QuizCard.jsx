494db9ccd46ef10e348ca19844d1942b1c3987fc
Carmani H. Jackson <charrisjackson422@gmail.com>
2025-02-25 20:35:03 -0500

    no changes just mc

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -5,6 +5,10 @@ const QuizCard = ({question}) => {
     // add state variable for the question and answer
     // pass question state as props to question card
     // update answer state from question card and pass to eval card
+const [setQuestion, updateQuestion] = useState('');
+const [setAnswer, updateAnswer] = useState('');
+
+
     return (
         <>
             <div className="mb-44 flex justify-center">
